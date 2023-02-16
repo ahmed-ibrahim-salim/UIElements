289996edc1583a1d8f5fee4c770327b5627ba7bc
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2023-02-16 16:52:16 +0200

    added new textfield

--- uiElements/Controllers/SearchViewController.swift
+++ uiElements/Controllers/SearchViewController.swift
@@ -11,14 +11,169 @@ import SnapKit
 
 class SearchViewController: UIViewController {
     
+    let mailTxtField: DesignableUITextField! = {
+        let txtField = DesignableUITextField()
+        
+        txtField.leadingImage = UIImage(systemName: "xmark")
+        txtField.leadingPadding = 10
+        txtField.imageTintColor = .black
+        txtField.placeholder = "Exambleemail@com"
+//        txtField.leadingPadding = 10
+
+        txtField.layer.borderColor = UIColor.systemGray5.cgColor
+        txtField.layer.borderWidth = 1
+        txtField.layer.cornerRadius = 10
+        return txtField
+    }()
     
+    let passTxtField: DesignableUITextField! = {
+        let txtField = DesignableUITextField()
+        
+        txtField.leadingImage = UIImage(systemName: "pencil.circle.fill")
+        txtField.leadingPadding = 10
+        txtField.imageTintColor = .black
+        txtField.isSecureTextEntry = true
+        txtField.placeholder = "•••••••••"
+
+//        txtField.leadingPadding = 10
+
+        txtField.layer.borderColor = UIColor.systemGray5.cgColor
+        txtField.layer.borderWidth = 1
+        txtField.layer.cornerRadius = 10
+        return txtField
+    }()
+    
+    var textFieldsStack = UIStackView()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         
+        
+        setConst()
+    }
+    
+    func setConst(){
+        
+        
+        textFieldsStack = UIStackView(arrangedSubviews: [mailTxtField, passTxtField])
+        textFieldsStack.axis = .vertical
+//        textFieldsStack.spacing = 10
+        
+        mailTxtField.snp.makeConstraints({ make in
+            make.height.equalToSuperview().multipliedBy(0.45)
+        })
+        passTxtField.snp.makeConstraints({ make in
+            make.height.equalToSuperview().multipliedBy(0.45)
+        })
+        
+        view.addSubview(textFieldsStack)
 
+        textFieldsStack.snp.makeConstraints({ make in
+            make.centerY.centerX.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.8)
+            make.height.equalToSuperview().multipliedBy(0.15)
+        })
         
         
     }
+}
+
+//MARK: Custom Text
+class DesignableUITextField: UITextField {
     
+    // Provides left padding for images
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.leftViewRect(forBounds: bounds)
+        textRect.origin.x += leadingPadding
+        return textRect
+    }
+    
+    // Provides right padding for images
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.rightViewRect(forBounds: bounds)
+        textRect.origin.x -= leadingPadding
+        return textRect
+    }
+    
+    @IBInspectable var leadingImage: UIImage? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var leadingPadding: CGFloat = 0
+    
+    @IBInspectable var imageTintColor: UIColor = UIColor.lightGray {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var rtl: Bool = false {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        rightViewMode = UITextField.ViewMode.never
+        rightView = nil
+        leftViewMode = UITextField.ViewMode.never
+        leftView = nil
+        
+        if let image = leadingImage {
+            var stack = UIStackView()
+            
+            let seperatorView = {
+                let vw = UIView()
+                vw.backgroundColor = .gray
+                return vw
+            }()
+            
+            
+            let imageView = {
+                let imageView = UIImageView()
+                imageView.image = image
+                imageView.contentMode = .scaleAspectFit
+                // Note: In order for your image to use the tint color, you have to select the image in the Assets.xcassets and change the "Render As" property to "Template Image".
+                imageView.tintColor = imageTintColor
+                return imageView
+            }()
+            
+            let spacingView = UIView()
+            
+            stack = UIStackView(arrangedSubviews: [imageView, seperatorView, spacingView])
+            stack.spacing = 2
+            
+            
+//            stack.layer.borderColor = UIColor.black.cgColor
+//            stack.layer.borderWidth = 1
+            
+            imageView.snp.makeConstraints({make in
+                make.width.equalToSuperview().multipliedBy(0.7)
+            })
+//            seperatorView.snp.makeConstraints({make in
+//                make.width.equalToSuperview().multipliedBy(0.05)
+//            })
+            
+            spacingView.snp.makeConstraints({make in
+                make.width.equalToSuperview().multipliedBy(0.1)
+            })
+
+            stack.snp.makeConstraints({ make in
+                make.width.equalTo(25)
+            })
+            
+            if rtl {
+                rightViewMode = UITextField.ViewMode.always
+                rightView = stack
+            } else {
+                leftViewMode = UITextField.ViewMode.always
+                leftView = stack
+            }
+        }
+        
+        // Placeholder text color
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedString.Key.foregroundColor: imageTintColor])
+    }
 }
