ab179f799e61ecf1ea3f75dd03e75afa534aefda
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2023-02-16 14:24:40 +0200

    aa

--- uiElements/SceneDelegate.swift
+++ uiElements/SceneDelegate.swift
@@ -6,15 +6,23 @@
 //
 
 import UIKit
+import AVFAudio
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
    
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        
+       
+        
+        
+        
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
