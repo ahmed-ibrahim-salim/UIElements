fcd3388d7fa1b8bda01d4eee4161ac7da3665e17
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2023-03-25 14:29:36 +0200

    fixed

--- uiElements/Controllers/DownloadsViewController.swift
+++ uiElements/Controllers/DownloadsViewController.swift
@@ -31,7 +31,7 @@ class DownloadsViewController: UIViewController {
         })
         
         circleViewContainer.snp.makeConstraints({ make in
-            make.width.equalToSuperview().multipliedBy(0.8)
+            make.width.equalToSuperview().multipliedBy(0.9)
             make.bottom.centerX.equalToSuperview()
 //                .offset(-40)
             make.height.equalToSuperview().multipliedBy(0.5)
@@ -55,10 +55,10 @@ struct Category{
 
 // MARK: Circles View
 class CirclesView: UIView {
-    
+
     var myColor = UIColor.red
     var borderColor = UIColor.black
-    
+
     var categories = [Category(id: 0, color: .green, name: "ahmed"),
                       Category(id: 1, color: .red, name: "ahmed"),
                       Category(id: 2, color: .blue, name: "ahmed"),
@@ -67,102 +67,116 @@ class CirclesView: UIView {
                       Category(id: 5, color: .systemMint, name: "ahmed"),
                       Category(id: 6, color: .systemTeal, name: "ahmed"),
                       Category(id: 7, color: .systemMint, name: "ahmed"),
-                      Category(id: 8, color: .systemGray, name: "ahmed"),
-                      Category(id: 9, color: .systemBrown, name: "ahmed"),
-                      Category(id: 10, color: .systemTeal, name: "ahmed"),
-                      Category(id: 11, color: .systemMint, name: "ahmed"),
+//                      Category(id: 8, color: .systemGray, name: "ahmed"),
+//                      Category(id: 9, color: .systemBrown, name: "ahmed"),
+//                      Category(id: 10, color: .systemTeal, name: "ahmed"),
+//                      Category(id: 11, color: .systemMint, name: "ahmed"),
 //                      Category(id: 12, color: .systemTeal, name: "ahmed"),
 //                      Category(id: 13, color: .systemMint, name: "ahmed"),
 
 ]
-    
+
     var usedRects: [CGRect] = []
     var usedDiameters: [CGFloat] = []
-    
+
     init() {
         super.init(frame: .zero)
         backgroundColor = .systemBackground
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         setNeedsDisplay()
     }
-    
 
-    
+
+
     // MARK: Draw
     override func draw(_ rect: CGRect) {
         usedRects = []
         usedDiameters = []
-                
+
         for category in categories {
-            
+
             //1. Circle Diameter
             var diameter: CGFloat = 0
-            
+
             //2. Circle Rect
             var rect: CGRect = .zero
-            
+
             if category.id == 0{
                 //1. Circle Diameter
                 diameter = getDiameter(isFirst: true)
-                
+
                 //2. Circle Rect
                 rect = getRect(diameter: diameter, isFirst: true)
-                
+
             }else{
-                
+
                 diameter = getDiameter()
-                
+
                 //2. Circle Rect
                 rect = getRect(diameter: diameter)
                 
+
             }
+            
+            
             
             //3. Circle Path
             let circlePath = UIBezierPath(roundedRect: rect, cornerRadius: diameter / 2)
-            
+
             //4. Circle Color
             category.color.setFill()
-            
+
             //5. Draw Circle
             circlePath.fill()
-            
+
             //6. Circle Border Color
             //            borderColor.setStroke()
-            
+
             //7. Draw Circle Border
             //            circlePath.stroke()
-            
+
         }
-        
-        
+
+
     }
-    
+
     func calculateMidCircleDiamter()->CGFloat{
-        
-        let diam: CGFloat = (bounds.width / 20)
-//        - CGFloat(Double(categories.count) * 3)
+
+        let diam: CGFloat = bounds.width / CGFloat(Double(categories.count) * 0.4)
 //        (CGFloat(categories.count) * 12)
-        
+
         return diam
     }
-    
+
     func calculateOtherCirclesDiamter()->CGFloat{
-        let diam = (bounds.width / 20)
-//        - CGFloat(Double(categories.count) * 3)
+        let diam = bounds.width / CGFloat(Double(categories.count) * 0.5)
+//        bounds.width * 0.01
+        //CGFloat(Double(categories.count) * 100)
 //        (CGFloat(categories.count) * 8)
-        
+
         return diam
     }
     // MARK: Diameter
-    func getDiameter(isFirst: Bool = false) -> CGFloat {
+    func getDiameter(isFirst: Bool = false, iterations: Int = 0) -> CGFloat {
         var diameter = CGFloat(0)
+
+        if iterations > 1000{
+//            usedRects = []
+            usedDiameters = []
+
+//            self.draw(CGRectZero)
+            diameter = calculateOtherCirclesDiamter() * 0.8
+            usedDiameters.append(diameter)
+
+            return diameter
+        }
         
         if isFirst{
             diameter = calculateMidCircleDiamter()
@@ -170,27 +184,37 @@ class CirclesView: UIView {
         }else{
             diameter = CGFloat(calculateOtherCirclesDiamter() + CGFloat(arc4random_uniform(10)))
         }
-        
+
         for usedDiameter in usedDiameters
         where usedDiameter == diameter {
-            return getDiameter()
+            return getDiameter(iterations: iterations + 1)
         }
         usedDiameters.append(diameter)
         return diameter
     }
-    
-    
+
+
     // MARK: Circle Rect
-    func getRect(diameter: CGFloat, isFirst: Bool = false) -> CGRect {
-        
+    func getRect(diameter: CGFloat, isFirst: Bool = false, iterations: Int = 0) -> CGRect {
+        print(iterations, "iterations")
         var posX: CGFloat = 0
         var posY: CGFloat = 0
-        
+
         // created a rect
         // always intersects
         var rect = CGRect(x: 0, y: 0, width: 0, height: 0)
+
+
+        if iterations > 1000{
+
+            rect = CGRect(x: posX - (diameter * 0.8), y: posY - (diameter * 0.8), width: (diameter * 0.8), height: (diameter * 0.8))
+            usedRects.append(rect)
+
+            return rect
+
+        }
         
-        
+
         if isFirst{
             posX = CGFloat(UInt32(bounds.midX))
 //            * 0.32
@@ -200,41 +224,41 @@ class CirclesView: UIView {
             rect = CGRect(x: posX - diameter/2, y: posY - diameter/2, width: diameter, height: diameter)
 
             print(posX, "first")
-            
+
         }else{
-            
+
         // https://stackoverflow.com/questions/30318002/spawning-a-circle-in-a-random-spot-on-screen
-            
+
             posX = CGFloat(arc4random_uniform(UInt32(bounds.maxX)))
             posY = CGFloat(arc4random_uniform(UInt32(bounds.maxY)))
-            
+
             print(posX, "randomWidth")
 
             rect = CGRect(x: posX - diameter, y: posY - diameter, width: diameter, height: diameter)
 
         }
-        
-        
+
+
         if checkOutOfBounds(rect: rect){
-            return getRect(diameter: diameter)
+            return getRect(diameter: diameter, iterations: iterations + 1)
 
         }
-        
+
 //      check intersection
         for usedRect in usedRects where usedRect.intersects(rect) {
             // recursive call crashes
 
-            return getRect(diameter: diameter)
+            return getRect(diameter: diameter, iterations: iterations + 1)
         }
-        
+
         usedRects.append(rect)
 
         return rect
     }
-    
+
     //MARK: Out of bounds
     func checkOutOfBounds(rect: CGRect)->Bool{
         return rect.minX < bounds.minX || rect.minY < bounds.minX || rect.maxX > bounds.maxX || rect.maxY > bounds.maxY
     }
-    
+
 }
